9dfb7a4afa51caa70c826393dd9c69f261a1c043
Jazib-Khan <jazib1khan@gmail.com>
2025-01-02 21:31:30 +0000

    Added Vue to the Project

--- laravel-app/resources/views/images/index.blade.php
+++ laravel-app/resources/views/images/index.blade.php
@@ -4,10 +4,12 @@
         <title>Image Uploader with Vue + Laravel</title>
         <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <div class="max-w-lg mx-auto mt-24">
             <h1 class="text-4xl font-bold text-center">Image Uploader</h1>
+            <div id="app"></div>
         </div>
     </body>
 </html>
